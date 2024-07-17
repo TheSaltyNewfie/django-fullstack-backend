5b90ad21dd53a1ecfa2ac497501c3e300e976af4
Tobias Dodge <tobydodge65@gmail.com>
2024-07-17 12:19:39 -0230

    Fixed some issues

--- backend_django/urls.py
+++ backend_django/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('backend.urls')),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
-    path('api-token-auth', obtain_auth_token, name='api_token_auth')
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth')
 ]
